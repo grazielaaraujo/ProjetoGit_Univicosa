861c534bd033cb4515f8d3cd85db8b01cde36af7
grazielaaraujo <100145647+grazielaaraujo@users.noreply.github.com>
2022-02-22 18:23:52 -0300

    Update projeto-univicosa.sql

--- projeto-sql/projeto-univicosa.sql
+++ projeto-sql/projeto-univicosa.sql
@@ -1,1 +1,13 @@
-SELECT * FROM `projeto-univicosa`.professor;
+
+CREATE TABLE `projeto-univicosa`.`professor` (
+  `idmatricula` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `telefone` VARCHAR(45) NULL,
+  `datanascimento` VARCHAR(45) NULL,
+  `datacadastro` VARCHAR(45) NULL,
+  `dataatualizacao` VARCHAR(45) NULL,
+  `senha` VARCHAR(45) NULL,
+  PRIMARY KEY (`idmatricula`));
+  
+  SELECT * FROM `projeto-univicosa`.professor;
